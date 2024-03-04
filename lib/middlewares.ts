761cddc33ba61882bc793a73b03c5ed234d65c67
princekumargit <prince1382002kumar@gmail.com>
2024-03-05 01:35:38 +0530

    added geturl and delete endpoint fixed auth

--- lib/middlewares.ts
+++ lib/middlewares.ts
@@ -3,22 +3,28 @@ import express from "express";
 
 export const authenticateToken = async (req: express.Request, res: express.Response, next: Function) => {
     const accessToken = req.headers.authorization?.split(' ')[1];
-
+    const {userId} = req.body;
+    if(!userId){
+        res.status(400).json({message: "userId is Required"});
+        return;
+    }
+    // console.log(accessToken);
     if (!accessToken) {
-        return res.status(401).json({ error: 'Access token is missing' });
+        return res.status(499).json({ error: 'Access token is missing' });
+        return;
     }
 
     try {
         const userResponse = await supabase.auth.getUser(accessToken);
         const user = userResponse.data.user;
         if (userResponse.error || !user ) {
-            return res.status(401).json({ error: 'Invalid access token' });
+            return res.status(498).json({ error: 'Invalid access token' });
+        }
+        if(user.id != userId){
+            res.status(401).json({message:"Access Token doesn't match with userId"})
         }
 
-        if(!user.confirmed_at){
-            return res.status(401).json({ error: 'Not Verified' });
-        }
-
+        // console.log("next");
         next();
     } catch (error) {
         console.error('Error verifying access token:', error);
